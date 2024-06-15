30f16e462f374979f663e94450da49e07448896f
lanerson <45043973+lanerson@users.noreply.github.com>
2024-06-15 00:51:23 -0300

    Update lig4.cpp

--- lig4.cpp
+++ lig4.cpp
@@ -29,7 +29,7 @@ public:
         }
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
-                tab[i][j] = '-';
+                tab[i][j] = '.';
     }
     ~Tabuleiro()
     {
@@ -51,6 +51,12 @@ public:
             cout << i;
         cout << endl;
     }
+    void reiniciarTabuleiro()
+    {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                tab[i][j] = '.';
+    }
     void alternarVez(int erro = 0)
     {
         vez = !vez;
@@ -62,7 +68,7 @@ public:
     {
         if (coluna < m && coluna >= 0)
         {
-            if (tab[n - 1][coluna] != '-')
+            if (tab[n - 1][coluna] != '.')
             {
                 cout << "Coluna totalmente preenchida" << endl;
                 return 0; // não tá
@@ -71,19 +77,25 @@ public:
         }
         return 0; // não tá
     };
+    int escolheColuna()
+    {
+        int coluna;
+        cout << "Digite uma coluna: ";
+        cin >> coluna;
+        return coluna;
+    }
     void realizaJogada()
     {
         exibirTabuleiro();
         alternarVez();
-        int coluna;
-        cout << "Digite uma coluna: ";
-        cin >> coluna;
+        int coluna = escolheColuna();
+
         if (verificaJogada(coluna - 1))
         {
             int aux = n;
             for (int i = n - 1; i >= 0; i--)
             {
-                if (tab[i][coluna - 1] != '-')
+                if (tab[i][coluna - 1] != '.')
                 {
                     break;
                 }
@@ -118,49 +130,58 @@ public:
     {
         p2 = _p2;
     }
-    void resultadoFinal()
+    void resultadoHorizontal()
     {
-        // horizontal
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m - 3; j++)
             {
                 if (tab[i][j] == 'X' && tab[i][j + 1] == 'X' && tab[i][j + 2] == 'X' && tab[i][j + 3] == 'X')
                     setP1(getP1() + 1);
-                if (tab[i][j] == 'O' && tab[i][j + 1] == 'O' && tab[i][j + 2] == 'O' && tab[i][j + 3] == 'O')
+                if (tab[i][j] == '0' && tab[i][j + 1] == '0' && tab[i][j + 2] == '0' && tab[i][j + 3] == '0')
                     setP2(getP2() + 1);
             }
         }
-        // vertical
+    }
+    void resultadoVertical()
+    {
         for (int j = 0; j < m; j++)
         {
             for (int i = 0; i < n - 3; i++)
             {
                 if (tab[i][j] == 'X' && tab[i + 1][j] == 'X' && tab[i + 2][j] == 'X' && tab[i + 3][j] == 'X')
                     setP1(getP1() + 1);
-                if (tab[i][j] == 'O' && tab[i + 1][j] == 'O' && tab[i + 2][j] == 'O' && tab[i + 3][j] == 'O')
+                if (tab[i][j] == '0' && tab[i + 1][j] == '0' && tab[i + 2][j] == '0' && tab[i + 3][j] == '0')
                     setP2(getP2() + 1);
             }
         }
-        // diagonal
+    }
+    void resultadoDiagonal()
+    {
         for (int i = 0; i < n - 3; i++)
         {
             for (int j = 0; j < m - 3; j++)
             {
                 if (tab[i][j] == 'X' && tab[i + 1][j + 1] == 'X' && tab[i + 2][j + 2] == 'X' && tab[i + 3][j + 3] == 'X')
                     setP1(getP1() + 1);
-                if (tab[i][j] == 'O' && tab[i + 1][j + 1] == 'O' && tab[i + 2][j + 2] == 'O' && tab[i + 3][j + 3] == 'O')
+                if (tab[i][j] == '0' && tab[i + 1][j + 1] == '0' && tab[i + 2][j + 2] == '0' && tab[i + 3][j + 3] == '0')
                     setP2(getP2() + 1);
                 if (tab[n - i - 1][j] == 'X' && tab[n - i - 2][j + 1] == 'X' && tab[n - i - 3][j + 2] == 'X' && tab[n - i - 4][j + 3] == 'X')
                     setP1(getP1() + 1);
-                if (tab[n - i - 1][j] == 'O' && tab[n - i - 2][j + 1] == 'O' && tab[n - i - 3][j + 2] == 'O' && tab[n - i - 4][j + 3] == 'O')
+                if (tab[n - i - 1][j] == '0' && tab[n - i - 2][j + 1] == '0' && tab[n - i - 3][j + 2] == '0' && tab[n - i - 4][j + 3] == '0')
                     setP2(getP2() + 1);
             }
         }
+    }
+    void resultadoFinal()
+    {
+        resultadoHorizontal();
+        resultadoVertical();
+        resultadoDiagonal();
     };
     void gerenciaJogo()
     {
-        cout << "O jogo começou, primeiro é o Jogador 1" << endl;
+        cout << "O jogo começou" << endl;
 
         while (colunaCheia < m)
         {
@@ -179,30 +200,61 @@ public:
     };
     void inserir(int m, int n) // coluna, linha
     {
-        char jogada = vez ? 'X' : 'O';
+        char jogada = vez ? 'X' : '0';
         tab[n][m] = jogada;
     }
 };
+
 class Lig4 : public Tabuleiro
 {
 public:
     Lig4() : Tabuleiro(7, 6)
     {
     }
+    int resultadoFinal()
+    {
+        enum
+        {
+            Continua = 0,
+            Vitoria_da_Cor1,
+            Vitoria_da_Cor2,
+            Empate
+        };
+        resultadoHorizontal();
+        resultadoVertical();
+        resultadoDiagonal();
+        if (!getP1() && !getP2())
+            return Continua;
+        if (getP1() > getP2())
+            return Vitoria_da_Cor1;
+        else if (getP1() < getP2())
+            return Vitoria_da_Cor2;
+        else
+            return Empate;
+    }
+
     void gerenciaJogo()
     {
-        cout << "Lig 4 tradicional!!" << endl;
-        cout << "O jogo começou, primeiro é o Jogador 1" << endl;
-        while (!getP1() && !getP2())
+        while (resultadoFinal() != 2)
         {
-            realizaJogada();
-            resultadoFinal();
+            setP1(0);
+            setP2(0);
+            reiniciarTabuleiro();
+            cout << "Lig 4 tradicional!!" << endl;
+            cout << "O jogo começou" << endl;
+            while (!resultadoFinal())
+            {
+                realizaJogada();
+            }
+            exibirTabuleiro();
+            if (getP1())
+                cout << "Jogador 1 venceu" << endl
+                     << endl;
+            else
+                cout << "Jogador 2 venceu" << endl
+                     << endl;
+            alternarVez(1);
         }
-        exibirTabuleiro();
-        if (getP1())
-            cout << "Jogador 1 venceu" << endl;
-        else
-            cout << "Jogador 2 venceu" << endl;
     }
 };
 
@@ -213,5 +265,6 @@ int main()
 
     Lig4 t2 = Lig4();
     t2.gerenciaJogo();
+
     return 0;
 }
